befdc79979b685d8c709e59afe7e501271b793ae
Berg, Roelof <Roelof.Berg@draeger.com>
2016-09-29 18:33:03 +0200

    Results of peer review.

--- Utf8Ucs2Converter.cpp
+++ Utf8Ucs2Converter.cpp
@@ -18,64 +18,65 @@
 
 #include <iostream>
 #include <string>
+#include <stdint.h>
 
-bool utf8CharToUcs2Char(const char* utf8Tok, wchar_t* ucs2Char, unsigned int* utf8TokLen)
+bool utf8CharToUcs2Char(const char* utf8Tok, wchar_t* ucs2Char, uint32_t* utf8TokLen)
 {
     //We do math, that relies on unsigned data types
-    const unsigned char* utf8TokUs = (unsigned char*) utf8Tok;
+    const unsigned char* utf8TokUs = reinterpret_cast<const unsigned char*>(utf8Tok);
 
     //Initialize return values for 'return false' cases.
     *ucs2Char = L'?';
     *utf8TokLen = 1;
 
     //Decode
-    if (utf8TokUs[0] < 0x80)
+    if (0x80 > utf8TokUs[0])
     {
         //Tokensize: 1 byte
-        *ucs2Char = (wchar_t) (utf8TokUs[0]);
+        *ucs2Char = static_cast<const wchar_t>(utf8TokUs[0]);
     }
-    else if ((utf8TokUs[0] & 0xE0) == 0xC0)
+    else if (0xC0 == (utf8TokUs[0] & 0xE0))
     {
         //Tokensize: 2 bytes
-        if ( (utf8TokUs[1] < 0x80) || (0x80 != (utf8TokUs[1] & 0xC0) ))
+        if ( 0x80 != (utf8TokUs[1] & 0xC0) )
         {
             return false;
         }
         *utf8TokLen = 2;
-        *ucs2Char = (wchar_t) (
+        *ucs2Char = static_cast<const wchar_t>(
                   (utf8TokUs[0] & 0x1F) << 6
                 | (utf8TokUs[1] & 0x3F)
             );
     }
-    else if ((utf8TokUs[0] & 0xF0) == 0xE0)
+    else if (0xE0 == (utf8TokUs[0] & 0xF0))
     {
         //Tokensize: 3 bytes
-        if (   ( (utf8TokUs[1] < 0x80) || (0x80 != (utf8TokUs[1] & 0xC0)) )
-		    || ( (utf8TokUs[2] < 0x80) || (0x80 != (utf8TokUs[2] & 0xC0)) )
+        if (   ( 0x80 != (utf8TokUs[1] & 0xC0) )
+		    || ( 0x80 != (utf8TokUs[2] & 0xC0) )
 		   )
         {
             return false;
         }
         *utf8TokLen = 3;
-        *ucs2Char = (wchar_t) (
+        *ucs2Char = static_cast<const wchar_t>(
                   (utf8TokUs[0] & 0x0F) << 12
                 | (utf8TokUs[1] & 0x3F) << 6
                 | (utf8TokUs[2] & 0x3F)
             );
     }
-    else if ((utf8TokUs[0] & 0xF8) == 0xF0)
+    else if (0xF0 == (utf8TokUs[0] & 0xF8))
     {
         //Tokensize: 4 bytes
         *utf8TokLen = 4;
         return false;                        //Character exceeds the UCS-2 range (UCS-4 would be necessary)
     }
-    else if ((utf8TokUs[0] & 0xFC) == 0xF8)
+    else if ((0xF8 == utf8TokUs[0] & 0xFC))
     {
         //Tokensize: 5 bytes
         *utf8TokLen = 5;
         return false;                        //Character exceeds the UCS-2 range (UCS-4 would be necessary)
     }
-    else if ((utf8TokUs[0] & 0xFE) == 0xFC)
+    else if (0xFC == (utf8TokUs[0] & 0xFE))
     {
         //Tokensize: 6 bytes
         *utf8TokLen = 6;
@@ -92,33 +93,33 @@ bool utf8CharToUcs2Char(const char* utf8Tok, wchar_t* ucs2Char, unsigned int* ut
 void ucs2CharToUtf8Char(const wchar_t ucs2Char, char* utf8Tok)
 {
     //We do math, that relies on unsigned data types
-    unsigned int ucs2CharValue = (unsigned int) (ucs2Char);   //The standard doesn't specify the signed/unsignedness of wchar_t
-    unsigned char* utf8TokUs = (unsigned char*) utf8Tok;
+    uint32_t ucs2CharValue = static_cast<uint32_t>(ucs2Char);   //The standard doesn't specify the signed/unsignedness of wchar_t
+    unsigned char* utf8TokUs = reinterpret_cast<unsigned char*>(utf8Tok);
 
     //Decode
-    if (ucs2CharValue < 0x80)
+    if (0x80 > ucs2CharValue)
     {
         //Tokensize: 1 byte
-        utf8TokUs[0] = ucs2CharValue;
+        utf8TokUs[0] = static_cast<unsigned char>(ucs2CharValue);
         utf8TokUs[1] = '\0';
     }
-    else if (ucs2CharValue >= 0x80 && ucs2CharValue < 0x800)
+    else if (0x800 > ucs2CharValue)
     {
         //Tokensize: 2 bytes
         utf8TokUs[2] = '\0';
-        utf8TokUs[1] = (0x80 | (ucs2CharValue & 0x3F));
+        utf8TokUs[1] = static_cast<unsigned char>(0x80 | (ucs2CharValue & 0x3F));
 		ucs2CharValue = (ucs2CharValue >> 6);
-        utf8TokUs[0] = (0xC0 | ucs2CharValue);
+        utf8TokUs[0] = static_cast<unsigned char>(0xC0 | ucs2CharValue);
     }
-    else if (ucs2CharValue >= 0x800)
+    else
     {
         //Tokensize: 3 bytes
         utf8TokUs[3] = '\0';
-        utf8TokUs[2] = (0x80 | (ucs2CharValue & 0x3F));
+        utf8TokUs[2] = static_cast<unsigned char>(0x80 | (ucs2CharValue & 0x3F));
 		ucs2CharValue = (ucs2CharValue >> 6);
-        utf8TokUs[1] = (0x80 | (ucs2CharValue & 0x3F));
+        utf8TokUs[1] = static_cast<unsigned char>(0x80 | (ucs2CharValue & 0x3F));
 		ucs2CharValue = (ucs2CharValue >> 6);
-        utf8TokUs[0] = (0xE0 | ucs2CharValue);
+        utf8TokUs[0] = static_cast<unsigned char>(0xE0 | ucs2CharValue);
     }
 }
 
@@ -131,7 +132,7 @@ std::wstring utf8ToUcs2(const std::string& utf8Str)
 
     while (end > cursor)
     {
-        unsigned int utf8TokLen = 0;
+        uint32_t utf8TokLen = 1;
         utf8CharToUcs2Char(cursor, &ucs2CharToStrBuf[0], &utf8TokLen);
         ucs2Result.append(ucs2CharToStrBuf);
         cursor += utf8TokLen;
@@ -149,7 +150,7 @@ std::string ucs2ToUtf8(const std::wstring& ucs2Str)
 
     while (end > cursor)
     {
-        int ucs2Char = *cursor;
+        const wchar_t ucs2Char = *cursor;
         ucs2CharToUtf8Char(ucs2Char, utf8Sequence);
         utf8Result.append(utf8Sequence);
         cursor++;
